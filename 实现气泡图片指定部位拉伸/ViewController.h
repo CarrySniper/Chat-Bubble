e077cd1cda7516ed48fd40f54d17198089d0c166
cjq002 <seejoys@sijiukejideMac-mini.local>
2016-06-07 14:30:07 +0800

    新建工程

---
+++ 实现气泡图片指定部位拉伸/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  实现气泡图片指定部位拉伸
+//
+//  Created by 思久科技 on 16/6/7.
+//  Copyright © 2016年 Seejoys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 实现气泡图片指定部位拉伸/ViewController.m
@@ -0,0 +1,78 @@
+//
+//  ViewController.m
+//  实现气泡图片指定部位拉伸
+//
+//  Created by 思久科技 on 16/6/7.
+//  Copyright © 2016年 Seejoys. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView1;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView2;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView3;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView4;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage *image = [UIImage imageNamed:@"bubble_image"];
+    
+    self.imageView1.contentMode = UIViewContentModeCenter;
+    self.imageView1.image = image;
+    
+    self.imageView2.contentMode = UIViewContentModeScaleAspectFit;
+    self.imageView2.image = image;
+    
+    self.imageView3.contentMode = UIViewContentModeScaleToFill;
+    self.imageView3.image = image;
+    
+    self.imageView4.contentMode = UIViewContentModeScaleAspectFit;
+    self.imageView4.image = image;
+    
+    //需要的效果图
+    CGFloat top = 15;       // 顶端盖高度
+    CGFloat bottom = 10 ;   // 底端盖高度
+    CGFloat left = 10;      // 左端盖宽度
+    CGFloat right = 30;     // 右端盖宽度
+    UIEdgeInsets insets = UIEdgeInsetsMake(top, left, bottom, right);
+    
+    //不让图片拉伸变形 端盖(end cap)的概念 拉伸UIEdgeInsets内区域
+    /*
+     NS_AVAILABLE_IOS(5_0);
+     - (UIImage *)resizableImageWithCapInsets:(UIEdgeInsets)capInsets
+     
+     */
+    
+    /*
+     NS_AVAILABLE_IOS(6_0);
+     - (UIImage *)resizableImageWithCapInsets:(UIEdgeInsets)capInsets
+     resizingMode:(UIImageResizingMode)resizingMode
+     
+     UIImageResizingModeStretch：拉伸模式，通过拉伸UIEdgeInsets指定的矩形区域来填充图片
+     UIImageResizingModeTile：平铺模式，通过重复显示UIEdgeInsets指定的矩形区域来填充图片
+     */
+    image = [image resizableImageWithCapInsets:insets
+                                  resizingMode:UIImageResizingModeTile];
+    
+    self.imageView.image = image;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
